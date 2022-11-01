a885c356bd24e6831edc10d6ebb70326f8449db4
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-01 10:31:13 +0100

    Added MQTT helper

---
+++ src/Shared/MQTT/mqtt.h
@@ -0,0 +1,7 @@
+#include <MQTT.h>
+
+#define MQTT_USERNAME "ardui"
+#define MQTT_PASSWORD "s1hif-xp!sT-qCuwu"
+#define MQTT_BROKER_ADDRESS "62.66.208.26"
+
+void setupMQTT(char* clientId, MQTTClientCallbackSimpleFunction messageReceivedCallback);
