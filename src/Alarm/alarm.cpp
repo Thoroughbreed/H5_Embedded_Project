941b5c2899c8ad43373a75d049634b6250dc0849
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-01 14:41:38 +0100

    setArmed over MQTT now works checkSensors loop added

--- src/Alarm/alarm.cpp
+++ src/Alarm/alarm.cpp
@@ -1,12 +1,57 @@
 #include "alarm.h"
 
+const char* clientId = MQTT_ALARM_CLIENT_ID;
+extern MQTTClient mqttClient;
+int alarmArmedState = 0;
+unsigned long checkSensorMillis = 0;
+
 void setupAlarm() {
+    Serial.begin(9600);
     setupRGB();
+    
+    while (!setupWiFi());
+    while (!setupMQTT((char*)clientId, onMessageReceived));
+    mqttClient.subscribe(MQTT_ARM_TOPIC);
 }
 
 void loopAlarm() {
-    delay(1000);
-    setRGB(255, 0, 0);
-    delay(1000);
-    setRGB(0, 0, 0);
+    mqttClient.loop();
+    if (alarmArmedState == ALARM_DISARMED) return;
+
+    if (millis() - checkSensorMillis > 5000) {
+        checkSensorMillis = millis();
+        checkSensors();
+    }
+}
+
+void checkSensors() {
+    checkPIR();
+    if (alarmArmedState == ALARM_FULLY_ARMED) {
+        checkMagnet();
+    }
+}
+void checkPIR() {
+
+}
+void checkMagnet() {
+    
+}
+void setArmed(String payload) {
+    if (payload.toInt() == ALARM_DISARMED) {
+        setRGB(0, 255, 0);
+        alarmArmedState = 0;
+    } else if (payload.toInt() == ALARM_PARTIALLY_ARMED) {
+        setRGB(255, 255, 0);
+        alarmArmedState = 1;
+    } else if (payload.toInt() == ALARM_FULLY_ARMED) {
+        setRGB(255, 0, 0);
+        alarmArmedState = 2;
+    }
+}
+
+void onMessageReceived(String &topic, String &payload) {
+    Serial.println("Incoming: " + topic + " Payload: " + payload);
+    if (topic == MQTT_ARM_TOPIC) {
+        setArmed(payload);
+    }
 }
--- src/Alarm/alarm.h
+++ src/Alarm/alarm.h
@@ -1,4 +1,19 @@
 #include "../Shared/RGB/rgb.h"
+#include "../Shared/WiFi/wifi.h"
+#include "../Shared/MQTT/mqtt.h"
+
+#define MQTT_ALARM_CLIENT_ID "House_Alarm"
+#define MQTT_ARM_TOPIC "home/alarm/status"
+
+#define ALARM_DISARMED 0
+#define ALARM_PARTIALLY_ARMED 1
+#define ALARM_FULLY_ARMED 2
 
 void setupAlarm();
 void loopAlarm();
+void setArmed(String payload);
+void onMessageReceived(String &topic, String &payload);
+
+void checkPIR();
+void checkMagnet();
+void checkSensors();
