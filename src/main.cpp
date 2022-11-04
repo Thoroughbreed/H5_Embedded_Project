31eccfd8f8e678a0336080af3e4df8461e01a152
Peter Hymøller <peterhym21@gmail.com>
2022-11-04 10:34:05 +0100

    Climate ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Adafruit_GFX.h>
+#include "Climate/climate.h"
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
+  setupClimate();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  loopClimate();
 }
