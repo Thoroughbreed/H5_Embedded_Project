7742afa9ffb85cc3e6065bb6409a7861aa35bc62
Jan Andreasen <jan@tved.it>
2022-12-16 08:34:03 +0100

    Changed broker IP

--- src/Shared/MQTT/mqtt.h
+++ src/Shared/MQTT/mqtt.h
@@ -5,8 +5,8 @@
 
 #define MQTT_USERNAME "ardui"
 #define MQTT_PASSWORD "s1hif-xp!sT-qCuwu"
-#define MQTT_BROKER_ADDRESS "62.66.208.26"
-
+//#define MQTT_BROKER_ADDRESS "62.66.208.26"
+#define MQTT_BROKER_ADDRESS "10.135.16.69"
 bool setupMQTT(char* clientId, MQTTClientCallbackSimpleFunction messageReceivedCallback);
 
 #endif
